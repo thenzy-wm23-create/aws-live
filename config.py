440e657f21372d87d7ac06627f92874a991611b5
thenzy-wm23-create <thenzy-wm23@student.tarc.edu.my>
2025-08-26 02:27:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cdhz8cthla4m.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "thenzhiyuan-employee"
+customregion = "us-east-1"
+
